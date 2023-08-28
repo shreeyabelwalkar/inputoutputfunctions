6ab195b8995c9c50ec41258513ff15fc7e38ec6f
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-28 13:59:55 +0530

    New file imput4.py is created

---
+++ input4.py
@@ -0,0 +1,5 @@
+n1, n2, n3=10, 20, 30
+print("number1={0}, number2={1}, number3={2}".format(n1,n2,n3))
+print("number1={1}, number2={2}, number3={2}".format(n1,n2,n3))
+print("number1={one}, number2={two}, number3={three}".format(one=n1,two=n2,three=n3))
+print("number1={a}, number2={b}, number3={c}".format(a=n1,b=n2,c=n3))
