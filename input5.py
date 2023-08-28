7c862e7c4103ab2f2dea3a13eeb1a0a999454026
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-28 14:21:27 +0530

    New file imput5.py is created

---
+++ input5.py
@@ -0,0 +1,21 @@
+str = input("enter any name:")
+print("The type of input str=",type (str))
+print("The user has entered",str)
+
+num1 = int(input("Enter First number:"))
+num2 = int(input("Enter Second number:"))
+print("The sum of num1 and num2=",num1+num2)
+
+num1 = float(input("Enter First number:"))
+num2 = float(input("Enter Second number:"))
+print("The sum of num1 and num2=",num1+num2)
+
+num1 = input("Enter First number:")
+num2 = input("Enter Second number:")
+print("The sum of num1 and num2=",int(num1+num2))
+
+num1 = input("Enter First number:")
+num2 = input("Enter Second number:")
+num3=int(num1)+int(num2)
+print("The sum of num1 and num2=%d"%(num3))
+
