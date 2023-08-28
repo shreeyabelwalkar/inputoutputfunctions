2906440f8e1e09306b48de061efee7344938ac7f
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-28 15:33:14 +0530

    New file multipleinpu.py is created

---
+++ multipleinpu.py
@@ -0,0 +1,5 @@
+num1,num2,num3,num4,num5 = [int (x) for x in input("Enter any 5 numbers:").split()]
+print("Sum=",(num1+num2+num3+num4+num5))
+
+num1,num2,num3,num4,num5 = [int (x) for x in input("Enter any 5 numbers:").split(",")]
+print("Sum=",(num1+num2+num3+num4+num5))
