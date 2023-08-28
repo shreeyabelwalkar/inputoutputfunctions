0c99f9f668e343c35dc4e8a173bfff6f639c823a
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-28 13:52:44 +0530

    New file imput3.py is modified

--- input3.py
+++ input3.py
@@ -11,3 +11,12 @@ print("num=%f"%num)
 print("num=%0.2f"%num)
 
 print("num=%5.3f"%num)
+
+name="Sagar"
+print("Hello %c, %c"%(name[0],name[1]))
+print('Hello %s'%(name[0:3]))
+
+num=124.35
+print("The value of num=%f"%num)
+print("The value of num=%.4f"%num)
+print("The value of num=%f.3"%num)ggg
