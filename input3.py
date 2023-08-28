9ea3168209e2c1c5efaf7b0f484dcd53278cb14a
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-28 12:24:43 +0530

    New file imput3.py is created

---
+++ input3.py
@@ -0,0 +1,13 @@
+name='Sagar'
+print("Hello %s"%name)
+
+print("Hello(%10s)"%name)
+
+print("Hello(%-10s)"%name)
+
+num=123.45
+print("num=%f"%num)
+
+print("num=%0.2f"%num)
+
+print("num=%5.3f"%num)
